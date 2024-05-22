e47112af4bbabfe993f9f1c47bfb8f9b90f7d730
caanges <36509245+caanges@users.noreply.github.com>
2024-05-22 12:21:07 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <nrfx.h>
+#include <nrf5340_application.h>
+#include <nrf.h>
+#include <nrfx_config.h>
+#include <nrfx_uarte.h>
+#include <nrfx_systick.h>
+#include <nrf_gpio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <nrfx_glue.h>
+#include <time.h>
+#include "game.h"
+#include <stdio.h>
+
+
+
+
+static nrfx_uarte_t instance = NRFX_UARTE_INSTANCE(0);
+
+
+int main(void)
+{
+    //start the programm
+    start_game_grupp_30();
+    return 0;
+}
+
